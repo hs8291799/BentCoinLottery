7f55ae9419552437f202fb0cd75eaa37ead766c6
Shuo Huang <shuoh@uchicago.edu>
2019-09-11 14:59:25 -0500

    Update: rbinom
    Using rbinom  to perform the same trial as origin

--- lottery.R
+++ lottery.R
@@ -16,15 +16,17 @@ Win <- 0.1
 UpperSide <- 1
 DownSide <- 0
 
-Lottery <- array(0,Num_of_flips)
-for (i in 1:Num_of_flips){
-  Lottery[i] <- if (runif(UpperSide) < Win) UpperSide else DownSide
-}
+##Lottery <- array(0,Num_of_flips)
+##for (i in 1:Num_of_flips){
+##  Lottery[i] <- if (runif(UpperSide) < Win) UpperSide else DownSide
+##}
 
-for (j in 1:Num_of_flips2){
-  Lottery <- array(0,Num_of_flips)
-  for (i in 1:Num_of_flips){
-    Lottery[i] <- if (runif(UpperSide) < Win) UpperSide else DownSide
-  }
-  print(Lottery)
-}
+##for (j in 1:Num_of_flips2){
+##  Lottery <- array(0,Num_of_flips)
+##  for (i in 1:Num_of_flips){
+##    Lottery[i] <- if (runif(UpperSide) < Win) UpperSide else DownSide
+##  }
+##  print(Lottery)
+##}
+
+rbinom(100,10,0.5)
